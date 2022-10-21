9772b0dd663f49ce442fb70074cf116cc80a0886
samjain2907 <jain.samarth2907@gmail.com>
2022-10-22 02:18:51 +0530

    adding routes and connecting database

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());    // takes the JSON data that is sent in a request and parses it and attaches it to the request object
+
+app.set('view engine', 'ejs');
+
+//database connection
+const dbURI = 'mongodb+srv://samarth:kJlxM9CBaLZK0vb7@cluster0.ztluc6u.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true,  useUnifiedTopology: true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+
+//routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes);
---
+++ routes/authRoutes.js
@@ -0,0 +1,14 @@
+const { Router } = require('express');
+const authController = require('../controllers/authController');
+
+const router = Router();      // creating a new instance of the router class
+
+router.get('/signup', authController.signup_get);
+router.post('/signup', authController.signup_post);
+router.get('/login', authController.login_get);
+router.post('/signup', authController.login_post);
+
+
+// since we are using the MVC approach we will be implementing the functions inside the authControl file.
+
+module.exports = router;
